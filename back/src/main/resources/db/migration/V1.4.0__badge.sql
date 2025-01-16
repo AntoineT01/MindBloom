8a371510cdc7daf104d68956d2c3164532b854c1
mathieuLeCalvez <163201926+mathieuLeCalvez@users.noreply.github.com>
2025-01-16 13:47:58 +0100

    fix bdd

--- back/src/main/resources/db/migration/V1.4.0__badge.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS badge (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    text VARCHAR(255) NOT NULL,
-    image LONGBLOB NULL
-)COMMENT = 'Representation of an account request in the database';
