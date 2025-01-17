e243c4799563e1711e94598396d24b2552d2ba91
Anthony Lottin <anthony.lottin.22@gmail.com>
2025-01-17 14:30:08 +0100

    Composant question paramètre

--- front/components/ReponseSlots.vue
+++ front/components/ReponseSlots.vue
@@ -1,43 +1,46 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
 import { ref } from 'vue';
+
+// Recevoir les props
+defineProps({
+  letter: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+});
 
 // État pour suivre si le bouton est cliqué ou non
 const isClicked = ref(false);
 
+// Fonction pour gérer le clic
 function handleClick() {
   isClicked.value = !isClicked.value;
   console.log('Button clicked! State:', isClicked.value);
 }
-
-
-// Objet réactif contenant les données de l'utilisateur
-const slot = reactive({
-    question: 'sqdfsdfsf sqdfqsdfsdfqsjdfkjqskdfjqsjkfkqsjfjqsdfqksjdfkqsjkdf',
-    letter: 'A'
-});
-
-
 </script>
 
-  
-
 <template>
-    <button 
-      type="button"
-      :class="[isClicked ? 'bg-purple-700' : 'bg-purple-500', 'inline-flex items-center rounded-3xl px-4 py-2 cursor-pointer transition-all duration-300 hover:bg-purple-600 focus:outline-none']"
-      @click="handleClick"
+  <button 
+    type="button"
+    :class="[isClicked ? 'bg-violet-ple'  : 'bg-violet-clair', 'inline-flex items-center rounded-3xl px-4 py-2 cursor-pointer  font-orbitron font-extrabold']"
+    @click="handleClick"
+  >
+    <!-- Cercle avec la lettre -->
+    <span
+      :class="[isClicked ? 'bg-violet-clair text-faux-blanc' : 'bg-violet-ple text-violet-fonc', 'shadow-xl rounded-full flex items-center justify-center text-xl font-bold']"
+      class="w-10 h-fit aspect-square"
     >
-      <!-- Cercle avec la lettre -->
-      <span 
-        :class="[isClicked ? 'bg-white text-black' : 'bg-purple-700 text-white','rounded-full  w-10 h-10 flex items-center justify-center text-xl font-bold transition-all duration-300 hover:scale-105 active:scale-95']"
-      >
-        A
-      </span>
-      <!-- Texte de la question -->
-      <span class="text-white font-medium ml-4">
-        Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the
-      </span>
-    </button>
-  </template>
-  
+      {{ letter }}
+    </span>
+
+    <!-- Texte de la question -->
+    <span 
+      :class="[isClicked ? 'text-violet-fonc':' text-faux-blanc', ' text-left font-medium ml-4']">
+      {{ question }}
+    </span>
+  </button>
+</template>
