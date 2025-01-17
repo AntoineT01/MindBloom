a9bc9e363ef30899d4f1f1f159241e1b36c2247c
Flintpop <tanguy.legoff2@gmail.com>
2025-01-17 14:38:24 +0100

    fix: V0.1.1 - On peut demander la création d'un compte. Fonctionne, testé de bout en bout sans front

--- back/src/main/java/com/tux/mindbloom/api/models/SignupDto.java
+++ back/src/main/java/com/tux/mindbloom/api/models/SignupDto.java
@@ -59,7 +59,8 @@ public class SignupDto {
   /**
    * An account's encrypted password, will be used for login credential
    */
-  @Schema(example = "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec", description = "An account's encrypted password, will be used for login credential")
+//  @Schema(example = "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec", description = "An account's encrypted password, will be used for login credential")
+  @Schema(example = "toto", description = "An account's encrypted password, will be used for login credential")
   @Size(min = 1, max = 255)
   @NotNull
   private String password;
