7e7e8198b5768110695b4ca93ca27fecb4ca8bbf
Anthony Lottin <anthony.lottin.22@gmail.com>
2025-02-07 14:24:20 +0100

    fix : taille

--- front/components/ReponseSlots.vue
+++ front/components/ReponseSlots.vue
@@ -26,21 +26,22 @@ function handleClick() {
 <template>
   <button 
     type="button"
-    :class="[isClicked ? 'bg-violet-ple'  : 'bg-violet-clair', 'inline-flex items-center rounded-3xl px-4 py-2 cursor-pointer  font-orbitron font-extrabold']"
+    :class="[isClicked ? 'bg-violet-ple' : 'bg-violet-clair', 'inline-flex items-center w-full rounded-3xl min-h-14 max-h-44 px-4 py-2 cursor-pointer font-orbitron font-extrabold']"
     @click="handleClick"
   >
     <!-- Cercle avec la lettre -->
     <span
-      :class="[isClicked ? 'bg-violet-clair text-faux-blanc' : 'bg-violet-ple text-violet-fonc', 'shadow-xl rounded-full flex items-center justify-center text-xl font-bold']"
-      class="w-10 h-fit aspect-square"
+      :class="[isClicked ? 'bg-violet-clair text-faux-blanc' : 'bg-violet-ple text-violet-fonc', 'shadow-xl rounded-full flex items-center justify-center text-xl font-bold flex-shrink-0 text-center leading-none']"
+      class="w-14 h-14"
     >
       {{ letter }}
     </span>
 
     <!-- Texte de la question -->
     <span 
-      :class="[isClicked ? 'text-violet-fonc':' text-faux-blanc', ' text-left font-medium ml-4']">
-      {{ question }}
+      :class="[isClicked ? 'text-violet-fonc' : 'text-faux-blanc', 'text-left font-medium ml-4']">
+      {{ question  }}
     </span>
   </button>
 </template>
+
