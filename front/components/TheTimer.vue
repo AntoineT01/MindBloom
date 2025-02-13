61c82579986292cd5eb1fbbff822f7e49353e790
Anthony Lottin <anthony.lottin.22@gmail.com>
2025-02-13 14:32:03 +0100

    add: TheTimer

---
+++ front/components/TheTimer.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="flex items-center w-full max-w-md">
+    <div class=" w-[90%] flex items-center bg-violet-fonc rounded-full">
+      <div class="w[50%] h-2 bg-violet-clair rounded-full" :style="{ width: progress +'%'}"></div>
+    </div>
+    <div class=" text-black font-bold">{{ formatTime(remainingTime) }}s</div>
+    </div>
+</template>
+  
+<script>
+export default {
+  props: {
+    duration: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      remainingTime: this.duration,
+      progress: 100, // Initialisation à 100%
+      interval: null,
+      startTime: null
+    };
+  },
+  mounted() {
+    this.startProgress();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    startProgress() {
+      this.startTime = performance.now();
+      this.interval = setInterval(() => {
+        const elapsed = (performance.now() - this.startTime) / 1000;
+        this.remainingTime = Math.max(this.duration - elapsed, 0);
+        this.progress = Math.max((this.remainingTime / this.duration) * 100, 0).toFixed(2); // Arrondi pour éviter les erreurs de flottants
+        if (this.remainingTime <= 0) {
+          clearInterval(this.interval);
+        }
+      }, 10);
+    },
+    formatTime(time) {
+      return `${time.toFixed(1)}`;
+    }
+  }
+};
+</script>
+
+
+  <style scoped>
+  </style>
+  
