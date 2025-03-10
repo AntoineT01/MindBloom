34bbdd956d268c919037db5d0a8dbc41ed46f491
mathieuLeCalvez <163201926+mathieuLeCalvez@users.noreply.github.com>
2025-03-10 09:15:39 +0100

    probleme pour lancer l'app en local mais les changement sont normalement ok, création d'une nouvelle branche pour éviter de tout casser

--- back/src/main/java/com/tux/mindbloom/dao/db/QuizSessionRepository.java
+++ back/src/main/java/com/tux/mindbloom/dao/db/QuizSessionRepository.java
@@ -4,10 +4,14 @@ import com.tux.mindbloom.dao.db.entities.QuizSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Accès à la base de données pour l'entité QuizSession.
  */
 @Repository
 public interface QuizSessionRepository extends JpaRepository<QuizSession, Long> {
-    // Méthodes de requête personnalisées si nécessaire.
+    Optional<QuizSession> findBySessionCode(String sessionCode);
+
 }
+
